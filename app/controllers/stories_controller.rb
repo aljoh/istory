3431d1ced8c964e8d59d17020f0144f2a47e19b8
aljoh <reachingalfred@gmail.com>
2017-12-10 21:49:45 +0100

    refactored, removed welcome controller, index action moved to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,10 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, only: [:show]
+
+  def index
+    @stories = Story.all
+  end
+
   def show
     @story = Story.find(params[:id])
     @first_chapter = @story.chapters.first
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: :omniauth_callbacks }
 
-  root controller: :welcome, action: :index
+  root controller: :stories, action: :index
 
   resources :stories do
     resources :chapters
